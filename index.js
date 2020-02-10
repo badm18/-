b8f20f9c057ae88d185cf4602ddc6b7651946261
Badm1 <shattilov@mail.ru>
2020-02-10 19:20:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onload=function(){
+
+
+    function palindrome(str) {
+        //
+      let tt=str;
+      str=tt.replace(/[\W_]/g,'');
+      tt=str.split('').reverse().join('');
+        
+        tt=tt.toLowerCase();
+       str= str.toLowerCase();
+
+    return str==tt
+
+
+
+
+
+      }
+      
+      
+      
+      console.log(palindrome("A man, a plan, a canal. Panama"));
+      
+
+
+
+}
